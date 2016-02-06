6ee3783c46023a1af3a5f1b13d5b57e7b341ff52
SaKa1979 <sanderkamps79@gmail.com>
2016-02-06 16:04:39 +0100

    Main GUI design done

--- src/gui/OVmain.java
+++ src/gui/OVmain.java
@@ -34,6 +34,7 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
 
 public class OVmain {
 
@@ -70,38 +71,35 @@ public class OVmain {
   private void initialize() {
     frmOvSimulator = new JFrame();
     frmOvSimulator.setResizable(false);
-    frmOvSimulator.setSize(new Dimension(1024, 768));
-    frmOvSimulator.setPreferredSize(new Dimension(1024, 768));
-    frmOvSimulator.getContentPane().setPreferredSize(new Dimension(1024, 768));
-    frmOvSimulator.getContentPane().setSize(new Dimension(1024, 768));
+//    frmOvSimulator.setSize(new Dimension(1024, 768));
     frmOvSimulator.setTitle("OV simulator");
     frmOvSimulator.setIconImage(Toolkit.getDefaultToolkit().getImage(OVmain.class.getResource("/images/SWARCOLOGO.jpeg")));
-    frmOvSimulator.setBounds(100, 100, 499, 341);
+    frmOvSimulator.setBounds(100, 100, 800, 600);
     frmOvSimulator.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     GridBagLayout gridBagLayout = new GridBagLayout();
-    gridBagLayout.columnWidths = new int[] {100, 0, 200};
-    gridBagLayout.rowHeights = new int[]{0, 222, 0};
-    gridBagLayout.columnWeights = new double[]{1.0, 1.0, 0.0};
-    gridBagLayout.rowWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+    gridBagLayout.columnWidths = new int[] {200, 600};
+    gridBagLayout.rowHeights = new int[] {500, 10};
+    gridBagLayout.columnWeights = new double[]{0.0, 0.0};
+    gridBagLayout.rowWeights = new double[]{0.0, 0.0};
     frmOvSimulator.getContentPane().setLayout(gridBagLayout);
     
     JPanel VehiclePanel = new JPanel();
     VehiclePanel.setToolTipText("Press the left mouse button to sent public transport message to the ITC.\r\nPress the right mouse button to configure this button.");
     VehiclePanel.setBorder(new TitledBorder(null, "Vehicle", TitledBorder.LEADING, TitledBorder.TOP, null, null));
     GridBagConstraints gbc_VehiclePanel = new GridBagConstraints();
-    gbc_VehiclePanel.insets = new Insets(0, 0, 5, 5);
+    gbc_VehiclePanel.insets = new Insets(5, 5, 5, 5);
     gbc_VehiclePanel.fill = GridBagConstraints.BOTH;
     gbc_VehiclePanel.gridx = 0;
     gbc_VehiclePanel.gridy = 0;
     frmOvSimulator.getContentPane().add(VehiclePanel, gbc_VehiclePanel);
-    VehiclePanel.setLayout(new GridLayout(1, 0, 0, 0));
+    VehiclePanel.setLayout(new GridLayout(10, 2, 5, 5));
     
     JPanel FeedbackPanel = new JPanel();
     FeedbackPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Feedback", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
     GridBagConstraints gbc_FeedbackPanel = new GridBagConstraints();
-    gbc_FeedbackPanel.insets = new Insets(0, 0, 5, 0);
+    gbc_FeedbackPanel.insets = new Insets(5, 0, 5, 5);
     gbc_FeedbackPanel.fill = GridBagConstraints.BOTH;
-    gbc_FeedbackPanel.gridx = 2;
+    gbc_FeedbackPanel.gridx = 1;
     gbc_FeedbackPanel.gridy = 0;
     frmOvSimulator.getContentPane().add(FeedbackPanel, gbc_FeedbackPanel);
     FeedbackPanel.setLayout(new GridLayout(0, 1, 0, 0));
@@ -110,21 +108,25 @@ public class OVmain {
     FeedbackPanel.add(scrollPane);
     
     JTextPane feedbackTxtpn = new JTextPane();
-    feedbackTxtpn.setText("Test text is here to stay!!!");
+    feedbackTxtpn.setText("Test text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!\r\nTest text is here to stay!!!");
     feedbackTxtpn.setBackground(SystemColor.inactiveCaptionBorder);
     scrollPane.setViewportView(feedbackTxtpn);
     
-    JPanel panel = new JPanel();
-    GridBagConstraints gbc_panel = new GridBagConstraints();
-    gbc_panel.anchor = GridBagConstraints.SOUTH;
-    gbc_panel.insets = new Insets(0, 0, 0, 5);
-    gbc_panel.gridx = 1;
-    gbc_panel.gridy = 1;
-    frmOvSimulator.getContentPane().add(panel, gbc_panel);
+    JPanel bottomInfoPanel = new JPanel();
+    bottomInfoPanel.setBorder(null);
+    FlowLayout flowLayout = (FlowLayout) bottomInfoPanel.getLayout();
+    flowLayout.setAlignment(FlowLayout.LEFT);
+    GridBagConstraints gbc_bottomInfoPanel = new GridBagConstraints();
+    gbc_bottomInfoPanel.fill = GridBagConstraints.HORIZONTAL;
+    gbc_bottomInfoPanel.gridx = 1;
+    gbc_bottomInfoPanel.gridy = 1;
+    frmOvSimulator.getContentPane().add(bottomInfoPanel, gbc_bottomInfoPanel);
     
     JTextPane txtpnSomeUberImportant = new JTextPane();
+    txtpnSomeUberImportant.setEditable(false);
+    txtpnSomeUberImportant.setSize(600, 10);
+    bottomInfoPanel.add(txtpnSomeUberImportant);
     txtpnSomeUberImportant.setText("Some uber important info here");
-    panel.add(txtpnSomeUberImportant);
     
     JMenuBar menuBar = new JMenuBar();
     frmOvSimulator.setJMenuBar(menuBar);
@@ -150,6 +152,9 @@ public class OVmain {
     JRadioButton rdbtnSics = new JRadioButton("SICS");
     protoButtonGroup.add(rdbtnSics);
     mnProtocol.add(rdbtnSics);
+    
+    JMenu mnAbout = new JMenu("About");
+    menuBar.add(mnAbout);
   }
 
   private static void addPopup(Component component, final JPopupMenu popup) {
